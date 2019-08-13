b574883b9ea725cccbfe20975dc7a2adeb39313f
totalhack <none@none.com>
2019-08-13 19:23:43 -0400

    File utility tweaks

--- toolbox/file_utils.py
+++ toolbox/file_utils.py
@@ -1,4 +1,13 @@
 import os
+import subprocess
 
-def rmfile(filename):
-    os.remove(filename)
+def rmfile(filename, ignore_missing=False):
+    try:
+        os.remove(filename)
+    except FileNotFoundError:
+        if ignore_missing:
+            return
+        raise
+
+def shell(cmd, shell=True, capture_output=True, **kwargs):
+    return subprocess.run(cmd, shell=shell, capture_output=capture_output, **kwargs)
